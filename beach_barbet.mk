c0f23795faaa0bcb488d88a762357c9b67d9b898
root <Beach OS>
2023-01-11 08:05:42 +0000

    BeachOS

---
+++ beach_barbet.mk
@@ -0,0 +1,22 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/beach/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/barbet/device-beach.mk)
+$(call inherit-product, device/google/redbull/device-beach.mk)
+$(call inherit-product, device/google/barbet/aosp_barbet.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := beach_barbet
+PRODUCT_MODEL := Pixel 5a
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=barbet \
+    PRIVATE_BUILD_DESC="barbet-user 13 TQ1A.230105.001 8672226 release-keys"
+
+BUILD_FINGERPRINT := google/barbet/barbet:13/TQ1A.230105.001/8672226:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+$(call inherit-product, vendor/google/barbet/barbet-vendor.mk)
